e592d24fda66a30d21b681f9484df168aa2696d7
Koen Ekelschot <koen.ekelschot@gmail.com>
2020-12-12 15:46:50 +0100

    Update deploy

--- build/deploy-docker.sh
+++ build/deploy-docker.sh
@@ -6,4 +6,4 @@ apk update && apk add curl openssh sshpass
 # upload .HA_VERSION to server and trigger build for Docker project
 export SSHPASS=$SSH_PASS
 sshpass -e scp -o StrictHostKeyChecking=no -r .HA_VERSION $SSH_USER:$SSH_FOLDER/.HA_VERSION
-curl -X POST -F token=f76dd8f0fb13505899a73ffea8b858 -F ref=master $CI_SERVER_URL/api/v4/projects/2/trigger/pipeline
+curl -X POST -F token=983463a64ea331786f9b2f31ec0acd -F ref=master $CI_SERVER_URL/api/v4/projects/10/trigger/pipeline
