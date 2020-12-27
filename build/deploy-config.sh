030824c8003948ff6c35a55d591a5ab4ab4de4a7
Koen Ekelschot <koen.ekelschot@gmail.com>
2020-12-27 14:50:39 +0100

    Install HACS

--- build/deploy-config.sh
+++ build/deploy-config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 copy_folder() {
-    sshpass -e ssh -o StrictHostKeyChecking=no $SSH_USER "test -d $SSH_FOLDER_HASS/$1 && rm -r -v $SSH_FOLDER_HASS/$1"
+    sshpass -e ssh -o StrictHostKeyChecking=no $SSH_USER "test -d $SSH_FOLDER_HASS/$1 && rm -r $SSH_FOLDER_HASS/$1"
     echo "Copying folder $1"
     sshpass -e scp -o StrictHostKeyChecking=no -r $1 $SSH_USER:$SSH_FOLDER_HASS/$1
 }
@@ -11,11 +11,21 @@ copy_file() {
     sshpass -e scp -o StrictHostKeyChecking=no $1 $SSH_USER:$SSH_FOLDER_HASS/$1
 }
 
+install_hacs() {
+    test -d $1 || mkdir -p $1
+    wget "https://github.com/hacs/integration/releases/latest/download/hacs.zip"
+    unzip "hacs.zip" -d $1
+    rm "hacs.zip"
+    copy_folder $1
+}
+
 # https://unix.stackexchange.com/a/417661
 apk update && apk add curl openssh sshpass
 
 export SSHPASS=$SSH_PASS
 
+install_hacs "custom_components/hacs"
+copy_folder "custom_components/denonavr"
 copy_folder "packages"
 copy_file "automations.yaml"
 copy_file "configuration.yaml"
