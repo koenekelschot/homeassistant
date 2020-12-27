b591e16c4a77753eb2d77556f764055c47e62fb2
Koen Ekelschot <koen.ekelschot@gmail.com>
2020-12-27 16:05:53 +0100

    Add themes

--- build/deploy-config.sh
+++ build/deploy-config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 copy_folder() {
-    sshpass -e ssh -o StrictHostKeyChecking=no $SSH_USER "test -d $SSH_FOLDER_HASS/$1 && rm -r $SSH_FOLDER_HASS/$1"
+    sshpass -e ssh -o StrictHostKeyChecking=no $SSH_USER "test -d $SSH_FOLDER_HASS/$1 && echo $SSH_PASS | sudo -S rm -r $SSH_FOLDER_HASS/$1"
     echo "Copying folder $1"
     sshpass -e scp -o StrictHostKeyChecking=no -r $1 $SSH_USER:$SSH_FOLDER_HASS/$1
 }
@@ -24,12 +24,18 @@ apk update && apk add curl openssh sshpass
 
 export SSHPASS=$SSH_PASS
 
-install_hacs "custom_components/hacs"
-copy_folder "custom_components/denonavr"
+#install hacs
+test -d "custom_components/hacs" || mkdir -p "custom_components/hacs"
+wget "https://github.com/hacs/integration/releases/latest/download/hacs.zip"
+unzip "hacs.zip" -d "custom_components/hacs"
+rm "hacs.zip"
+
+copy_folder "custom_components"
 copy_folder "packages"
 copy_file "automations.yaml"
 copy_file "configuration.yaml"
 copy_file "groups.yaml"
+copy_file "lovelace.yaml"
 copy_file "scenes.yaml"
 copy_file "scripts.yaml"
 
