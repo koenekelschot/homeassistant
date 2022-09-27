852096de6b34508cbdaeb3587b374a3bb96eb11b
Koen Ekelschot <koen.ekelschot@gmail.com>
2022-09-27 12:05:20 +0200

    remove hacs

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,32 +1,25 @@
 #!/bin/bash
 
 copy_folder() {
-    sshpass -e ssh -o StrictHostKeyChecking=no $SSH_USER "test -d $SSH_FOLDER_HASS/$1 && echo $SSH_PASS | sudo -S rm -r $SSH_FOLDER_HASS/$1"
+    sshpass -e ssh -o StrictHostKeyChecking=no $SSH_USER@$SSH_HOST "test -d $SSH_FOLDER_HASS/$1 && echo $SSH_PASS | sudo -S rm -r $SSH_FOLDER_HASS/$1"
     echo "Copying folder $1"
-    sshpass -e scp -o StrictHostKeyChecking=no -r $1 $SSH_USER:$SSH_FOLDER_HASS/$1
+    sshpass -e scp -O -o StrictHostKeyChecking=no -r $1 $SSH_USER@$SSH_HOST:$SSH_FOLDER_HASS/$1
 }
 
 copy_file() {
     echo "Copying file $1"
-    sshpass -e scp -o StrictHostKeyChecking=no $1 $SSH_USER:$SSH_FOLDER_HASS/$1
+    sshpass -e scp -O -o StrictHostKeyChecking=no $1 $SSH_USER@$SSH_HOST:$SSH_FOLDER_HASS/$1
 }
 
 export SSHPASS=$SSH_PASS
 
 #copy .HA_VERSION to main Docker folder
-sshpass -e scp -o StrictHostKeyChecking=no .HA_VERSION $SSH_USER:$SSH_FOLDER_DOCKER/.HA_VERSION
-
-#install hacs
-test -d "custom_components/hacs" || mkdir -p "custom_components/hacs"
-wget "https://github.com/hacs/integration/releases/latest/download/hacs.zip"
-unzip "hacs.zip" -d "custom_components/hacs"
-rm "hacs.zip"
+sshpass -e scp -O -o StrictHostKeyChecking=no .HA_VERSION $SSH_USER@$SSH_HOST:$SSH_FOLDER_DOCKER/.HA_VERSION
 
 copy_folder "custom_components"
 copy_folder "packages"
 copy_file "automations.yaml"
 copy_file "configuration.yaml"
 copy_file "groups.yaml"
-copy_file "lovelace.yaml"
 copy_file "scenes.yaml"
 copy_file "scripts.yaml"
